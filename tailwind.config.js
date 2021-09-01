1d43eeb1acf1badf0e0c7320924617615002c004
John Eatmon <jmaeatmon@icloud.com>
2021-08-31 22:04:30 -0700

    config: fluid typography config

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,5 +23,64 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-fluid')({
+      textSizes: {
+        xs: {
+          min: '0.87rem', //
+          max: '0.96rem',
+          minvw: '320px',
+          maxvw: '1140px',
+        },
+        sm: {
+          min: '1.04rem',
+          max: '1.15rem',
+          minvw: '320px',
+          maxvw: '1140px',
+        },
+        md: {
+          min: '1.25rem',
+          max: '1.38rem',
+          minvw: '320px',
+          maxvw: '1140px',
+        },
+        lg: {
+          min: '1.50rem',
+          max: '1.65rem',
+          minvw: '320px',
+          maxvw: '1140px',
+        },
+        xl: {
+          min: '1.80rem',
+          max: '1,98rem',
+          minvw: '320px',
+          maxvw: '1140px',
+        },
+        '2xl': {
+          min: '2.16rem',
+          max: '2.38rem',
+          minvw: '320px',
+          maxvw: '1140px',
+        },
+        '3xl': {
+          min: '2.59rem',
+          max: '2.85rem',
+          minvw: '320px',
+          maxvw: '1140px',
+        },
+        '4xl': {
+          min: '3.11rem',
+          max: '3.42rem',
+          minvw: '320px',
+          maxvw: '1140px',
+        },
+        '5xl': {
+          min: '3.73rem',
+          max: '4.11rem',
+          minvw: '320px',
+          maxvw: '1140px',
+        },
+      },
+    }),
+  ],
 };
