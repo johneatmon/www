9f9bb12fe337d404a7a2ac99cf8910bb3936d523
John Eatmon <jmaeatmon@icloud.com>
2021-09-07 19:29:00 -0700

    feat: colors! 🌈

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const { colors } = require('tailwindcss/defaultTheme');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
@@ -12,6 +13,61 @@ module.exports = {
       },
       screens: {
         xl: '1080px',
+      },
+    },
+    colors: {
+      inherit: 'inherit',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      cyan: {
+        50: 'var(--jm-cyan-50)',
+        100: 'var(--jm-cyan-100)',
+        200: 'var(--jm-cyan-200)',
+        300: 'var(--jm-cyan-300)',
+        400: 'var(--jm-cyan-400)',
+        500: 'var(--jm-cyan-500)',
+        600: 'var(--jm-cyan-600)',
+        700: 'var(--jm-cyan-700)',
+        800: 'var(--jm-cyan-800)',
+        900: 'var(--jm-cyan-900)',
+      },
+      pink: {
+        50: 'var(--jm-magenta-50)',
+        100: 'var(--jm-magenta-100)',
+        200: 'var(--jm-magenta-200)',
+        300: 'var(--jm-magenta-300)',
+        400: 'var(--jm-magenta-400)',
+        500: 'var(--jm-magenta-500)',
+        600: 'var(--jm-magenta-600)',
+        700: 'var(--jm-magenta-700)',
+        800: 'var(--jm-magenta-800)',
+        900: 'var(--jm-magenta-900)',
+      },
+      indigo: {
+        50: 'var(--jm-indigo-50)',
+        100: 'var(--jm-indigo-100)',
+        200: 'var(--jm-indigo-200)',
+        300: 'var(--jm-indigo-300)',
+        400: 'var(--jm-indigo-400)',
+        500: 'var(--jm-indigo-500)',
+        600: 'var(--jm-indigo-600)',
+        700: 'var(--jm-indigo-700)',
+        800: 'var(--jm-indigo-800)',
+        900: 'var(--jm-indigo-900)',
+      },
+      gray: {
+        50: 'var(--jm-blueGray-50)',
+        100: 'var(--jm-blueGray-100)',
+        200: 'var(--jm-blueGray-200)',
+        300: 'var(--jm-blueGray-300)',
+        400: 'var(--jm-blueGray-400)',
+        500: 'var(--jm-blueGray-500)',
+        600: 'var(--jm-blueGray-600)',
+        700: 'var(--jm-blueGray-700)',
+        800: 'var(--jm-blueGray-800)',
+        900: 'var(--jm-blueGray-900)',
       },
     },
     extend: {
