7a748486d3ea48abac4d0f239cb8214e61a03404
johnnytest4real <jmaeatmon@icloud.com>
2022-02-12 12:39:26 -0800

    config: register astro-imagetools plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
+import { astroImagePlugin } from 'astro-imagetools';
+
 export default {
   renderers: [],
   buildOptions: {
     site: 'https://johneatmon.dev',
     sitemap: true,
+  },
+  vite: {
+    plugins: [astroImagePlugin],
   },
 };
