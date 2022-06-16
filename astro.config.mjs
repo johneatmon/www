654a2943b3506765a183cea9fced1bb4a20251a4
johnnytest4real <jmaeatmon@icloud.com>
2022-06-15 20:06:59 -0700

    config: enable astro/postcss integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 import { defineConfig } from 'astro/config';
+import { astroImageTools } from 'astro-imagetools';
+import compress from 'astro-compress';
+import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
-  site: 'https://jmae.so',
+	site: 'https://jmae.xyz',
+	experimental: {
+		integrations: true,
+	},
+	integrations: [astroImageTools, compress({ html: false, img: false, svg: false }), robotsTxt()],
+	vite: {
+		ssr: {
+			external: ['svgo'],
+		},
+	},
 });
---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+const postcssPresetEnv = require('postcss-preset-env');
+
+module.exports = {
+	plugins: [
+		postcssPresetEnv({
+			stage: 2,
+			features: {
+				'nesting-rules': true,
+			},
+		}),
+	],
+};
