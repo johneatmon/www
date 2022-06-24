833c88bf6ea1ab9c1999697a5d8e0192959b5160
johnnytest4real <jmaeatmon@icloud.com>
2022-06-24 14:26:45 -0700

    config: enable astro-sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import { astroImageTools } from 'astro-imagetools';
 import compress from 'astro-compress';
 import robotsTxt from 'astro-robots-txt';
+import sitemap from 'astro-sitemap';
 
 export default defineConfig({
 	site: 'https://jmae.xyz',
@@ -11,7 +12,12 @@ export default defineConfig({
 	markdown: {
 		syntaxHighlight: 'prism',
 	},
-	integrations: [astroImageTools, compress({ html: false, img: false, svg: false }), robotsTxt()],
+	integrations: [
+		astroImageTools,
+		compress({ html: false, img: false, svg: false }),
+		robotsTxt(),
+		sitemap(),
+	],
 	vite: {
 		ssr: {
 			external: ['svgo'],
