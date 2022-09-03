deec0e99c905fdcd2aed3172c08b7519b144a935
jmaeat <jmaeatmon@icloud.com>
2022-09-03 16:10:46 -0700

    config: enable `mdx` integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jmae.xyz',
-	integrations: [image()],
+	integrations: [image(), mdx()],
 	vite: {
 		ssr: {
 			external: ['svgo'],
