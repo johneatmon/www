1f7361bcda387077d52664f9f0675490996706d7
jmaeat <jmaeatmon@icloud.com>
2022-09-06 18:26:28 -0700

    config: enable all astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,28 @@
 import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
+import critters from 'astro-critters';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
+import prefetch from '@astrojs/prefetch';
+import robotsTxt from 'astro-robots-txt';
+import sitemap from 'astro-sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jmae.xyz',
-	integrations: [image(), mdx()],
+	integrations: [
+		compress({
+			html: false,
+			img: false,
+			svg: false,
+		}),
+		critters(),
+		image(),
+		mdx(),
+		prefetch(),
+		robotsTxt(),
+		sitemap(),
+	],
 	vite: {
 		ssr: {
 			external: ['svgo'],
