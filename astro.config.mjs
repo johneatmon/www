26f58e39a04e4340bb7885e79c43f33051b0d10b
jmaeat <jmaeatmon@icloud.com>
2022-09-11 09:50:12 -0700

    config: enable `astro-fonts-next` integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import compress from 'astro-compress';
 import critters from 'astro-critters';
+import fonts from 'astro-fonts-next';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import prefetch from '@astrojs/prefetch';
@@ -17,6 +18,9 @@ export default defineConfig({
 			svg: false,
 		}),
 		critters(),
+		fonts({
+			url: 'https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,400;0,6..72,700;1,6..72,400;1,6..72,700&display=swap',
+		}),
 		image(),
 		mdx(),
 		prefetch(),
